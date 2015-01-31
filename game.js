ead3aa6d18f19b3de844d7a68ee8b0ed9026c141
Tyler Schlegel <schleg@gmail.com>
2015-01-31 10:37:54 -0800

    player as a square, dog

--- game.js
+++ game.js
@@ -7,10 +7,17 @@
       x: canvas.width,
       y: canvas.height
     };
+    var entities = [];
     var update = function () {
+      entities.forEach(function (entity) {
+        entity.update(size);
+      });
     };
     var draw = function (screen, size) {
       screen.clearRect(0, 0, size.x, size.y);
+      entities.forEach(function (entity) {
+        entity.draw(screen);
+      });
     };
     var tick = function () {
       update();
@@ -18,8 +25,26 @@
       requestAnimationFrame(tick);
     };
     tick();
+    this.createEntity = function (type) {
+      entities.push(new type(self));
+    };
+  };
+  var Player = function (game) {
+    this.game = game;
+    this.size = { x: 10, y: 10 };
+    var center = { x: 0, y: 0 };
+    this.update = function (bounds) {
+      center = { x: bounds.x / 2, y: bounds.y - this.size.y * 2 }
+    };
+    this.draw = function (screen) {
+      screen.fillRect(
+        center.x - this.size.x / 2,
+        center.y - this.size.y / 2,
+        this.size.x, this.size.y);
+    };
   };
   window.addEventListener('load', function () {
     var game = new Game();
+    game.createEntity(Player);
   });
 })();
